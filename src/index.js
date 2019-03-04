15fa3d544e6b4a6652b8c7b408a911fe0e0af2c7
Jannis Hajda <jannis.hajda@protonmail.com>
2019-03-04 15:42:38 +0100

    Covered Player, User Profile, Follow API

--- src/index.js
+++ src/index.js
@@ -308,6 +308,412 @@ class SpotifyWebApi {
 
   /**
    *
+   * Player API
+   *
+   */
+
+  /**
+   * Get the User's Currently Playing Track
+   * Get the object currently being played on the user’s Spotify account.
+   *
+   * @param {String} market
+   */
+
+  async getCurrentPlaying(market) {
+    return this.performRequest({
+      url: `/me/player/currently-playing`,
+      params: {
+        market
+      }
+    });
+  }
+
+  /**
+   * Set Volume For User's Playback
+   * Set the volume for the user’s current playback device.
+   *
+   * @param {Integer} volume_percent
+   * @param {String} device_id
+   */
+
+  async setVolume(volume_percent = 50, device_id) {
+    return this.performRequest({
+      url: "/me/player/volume",
+      method: "PUT",
+      params: {
+        volume_percent,
+        device_id
+      }
+    });
+  }
+
+  /**
+   * Get Information About The User's Current Playback
+   * Get information about the user’s current playback state, including track, track progress, and active device.
+   *
+   * @param {String} market
+   */
+
+  async getPlaybackInfo(market) {
+    return this.performRequest({
+      url: "/me/player",
+      params: {
+        market
+      }
+    });
+  }
+
+  /**
+   * Skip User’s Playback To Previous Track
+   * Skips to previous track in the user’s queue.
+   *
+   * @param {String} device_id
+   */
+
+  async skipPrevious(device_id) {
+    return this.performRequest({
+      url: "/me/player/previous",
+      method: "POST",
+      params: {
+        device_id
+      }
+    });
+  }
+
+  /**
+   * Get Current User's Recently Played Tracks
+   * Get tracks from the current user’s recently played tracks.
+   *
+   * @param {Integer} limit
+   * @param {Integer} after
+   * @param {Integer} before
+   */
+
+  async getRecentlyPlayed(limit = 20, after, before) {
+    return this.performRequest({
+      url: "/me/player/recently-played",
+      params: {
+        limit,
+        after,
+        before
+      }
+    });
+  }
+
+  /**
+   * Skip User’s Playback To Next Track
+   * Skips to next track in the user’s queue.
+   *
+   * @param {String} device_id
+   */
+
+  async skipNext(device_id) {
+    return this.performRequest({
+      url: "/me/player/next",
+      method: "POST",
+      params: {
+        device_id
+      }
+    });
+  }
+
+  /**
+   * Pause a User's Playback
+   * Pause playback on the user’s account.
+   *
+   * @param {String} device_id
+   */
+
+  async pausePlayback(device_id) {
+    return this.performRequest({
+      url: "/me/player/pause",
+      method: "PUT",
+      params: {
+        device_id
+      }
+    });
+  }
+
+  /**
+   * Set Repeat Mode On User’s Playback
+   * Set the repeat mode for the user’s playback. Options are repeat-track, repeat-context, and off.
+   *
+   * @param {String} state
+   * @param {String} device_id
+   */
+
+  async setRepeatMode(state = "track", device_id) {
+    return this.performRequest({
+      url: "/me/player/repeat",
+      method: "PUT",
+      params: {
+        state,
+        device_id
+      }
+    });
+  }
+
+  /**
+   * Start/Resume a User's Playback
+   * Start/Resume a User's Playback
+   *
+   * @param {String} device_id
+   * @param {String} context_uri
+   * @param {Array} uris
+   * @param {Object} offset
+   * @param {integer} position_ms
+   */
+
+  async startPlayback(device_id, context_uri, uris = [], offset, position_ms) {
+    return this.performRequest({
+      url: "/me/player/play",
+      method: "PUT",
+      params: {
+        device_id
+      },
+      data: {
+        context_uri,
+        uris,
+        offset,
+        position_ms
+      }
+    });
+  }
+
+  /**
+   * Seek To Position In Currently Playing Track
+   * Seeks to the given position in the user’s currently playing track.
+   *
+   * @param {Integer} position_ms
+   * @param {String} device_id
+   */
+
+  async seekTo(position_ms = 0, device_id) {
+    return this.performRequest({
+      url: "/me/player/seek",
+      method: "PUT",
+      params: {
+        position_ms,
+        device_id
+      }
+    });
+  }
+
+  /**
+   * Transfer a User's Playback
+   * Transfer playback to a new device and determine if it should start playing.
+   *
+   * @param {Array} device_ids
+   * @param {Boolean} play
+   */
+
+  async transferPlayback(device_ids, play) {
+    return this.performRequest({
+      url: "/me/player",
+      method: "PUT",
+      data: {
+        device_ids,
+        play
+      }
+    });
+  }
+
+  /**
+   * Toggle Shuffle For User’s Playback
+   * Toggle shuffle on or off for user’s playback.
+   *
+   * @param {Boolean} state
+   * @param {String} device_id
+   */
+
+  async toggleShuffle(state, device_id) {
+    return this.performRequest({
+      url: "/me/player/shuffle",
+      method: "PUT",
+      params: {
+        state,
+        device_id
+      }
+    });
+  }
+
+  /**
+   * Get a User's Available Devices
+   * Get information about a user’s available devices.
+   */
+
+  async getAvailableDevices() {
+    return this.performRequest({
+      url: "/me/player/devices"
+    });
+  }
+
+  /**
+   *
+   * User Profile
+   *
+   */
+
+  /**
+   * Get a User's Profile
+   * Get public profile information about a Spotify user.
+   *
+   * @param {String} user_id
+   */
+
+  async getUserProfile(user_id) {
+    return this.performRequest({
+      url: `/users/${user_id}`
+    });
+  }
+
+  /**
+   * Get Current User's Profile
+   * Get detailed profile information about the current user (including the current user’s username).
+   */
+
+  async getCurrentUser() {
+    return this.performRequest({
+      url: "/me"
+    });
+  }
+
+  /**
+   *
+   * Follow API
+   *
+   */
+
+  /**
+   * Get Following State for Artists/Users
+   * Check to see if the current user is following one or more artists or other Spotify users.
+   *
+   * @param {String} type
+   * @param {Array} ids
+   */
+
+  async getFollowingState(type, ids = []) {
+    return this.performRequest({
+      url: "/me/following/contains",
+      params: {
+        type,
+        ids: [ids].filter(Boolean).join(",")
+      }
+    });
+  }
+
+  /**
+   * Check if Users Follow a Playlist
+   * Check to see if one or more Spotify users are following a specified playlist.
+   *
+   * @param {String} playlist_id
+   * @param {Array} ids
+   */
+
+  async checkIfUsersFollowPlaylist(playlist_id, ids = []) {
+    return this.performRequest({
+      url: `/playlists/${playlist_id}/followers/contains`,
+      params: {
+        ids: [ids].filter(Boolean).join(",")
+      }
+    });
+  }
+
+  /**
+   * Follow Artists or Users
+   * Add the current user as a follower of one or more artists or other Spotify users.
+   *
+   * @param {String} type
+   * @param {Array} ids
+   */
+
+  async followUsersOrArtists(type, ids = []) {
+    return this.performRequest({
+      url: "/me/following",
+      method: "PUT",
+      params: {
+        type,
+        ids: [ids].filter(Boolean).join(",")
+      },
+      data: {
+        ids: ids
+      }
+    });
+  }
+
+  /**
+   * Follow a Playlist
+   * Add the current user as a follower of a playlist.
+   *
+   * @param {String} playlist_id
+   * @param {Boolean} visible - Public is a researved word
+   */
+
+  async followPlaylist(playlist_id, visible = true) {
+    return this.performRequest({
+      url: `/playlists/${playlist_id}/followers`,
+      method: "PUT",
+      data: { visible }
+    });
+  }
+
+  /**
+   * Get User's Followed Artists
+   * Get the current user’s followed artists.
+   *
+   * @param {String} type
+   * @param {Integer} limit
+   * @param {String} after
+   */
+
+  async getFollowedArtists(type = "artist", limit = 20, after) {
+    return this.performRequest({
+      url: "/me/following",
+      params: {
+        type,
+        limit,
+        after
+      }
+    });
+  }
+
+  /**
+   * Unfollow Artists or Users
+   * Remove the current user as a follower of one or more artists or other Spotify users.
+   *
+   * @param {String} type
+   * @param {Array} ids
+   */
+
+  async unfollowArtistsOrUsers(type, ids = []) {
+    return this.performRequest({
+      url: "/me/following",
+      method: "DELETE",
+      params: {
+        type,
+        ids: [ids].filter(Boolean).join(",")
+      },
+      data: {
+        ids: ids
+      }
+    });
+  }
+
+  /**
+   * Unfollow Playlist
+   * Remove the current user as a follower of a playlist.
+   *
+   * @param {String} playlist_id
+   */
+
+  async unfollowPlaylist(playlist_id) {
+    return this.performRequest({
+      url: `/playlists/${playlist_id}/followers`
+    });
+  }
+
+  /**
+   *
    * Search API
    *
    */
